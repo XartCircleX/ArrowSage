c1b4bb27c13af4b1fa425c2b0a11158779465470
XartCircleX <bryanalexisdelgadolopez1@gmail.com>
2023-11-25 19:08:15 -0600

    all data recived

--- src/controllers/PerfilController.js
+++ src/controllers/PerfilController.js
@@ -1,33 +1,58 @@
 const studentService = require('../services/studentService');
-const circularJson = require('circular-json');
 
 const renderPerfilPage = async (req, res) => {
+  console.log("User object:", req.user);
+  const studentId = req.user.id_student;
+  const studentName = req.user.fullname;
+  const studentEmail = req.user.email;
+
   try {
-    console.log("User object:", req.user);
-    const studentId = req.user.id_student;
-    console.log("Student ID:", studentId);
+    const groupResult = await studentService.getGroupByStudentId(studentId);
+    const specialtyResult = await studentService.getSpecialtyByStudentId(studentId);
+    const turnResult = await studentService.getTurnByStudentId(studentId);
+    const tutorResult = await studentService.getTutorByStudentId(studentId);
+    const periodResult = await studentService.getPeriodByStudentId(studentId);
+    const asignatureResult = await studentService.getAsignatureByStudentId(studentId);
 
-    // Obtén el grupo directamente de la base de datos
-    const group = await studentService.getGroupByStudentId(studentId);
-    
-    // Convierte el grupo a JSON con manejo de referencias circulares
-    const jsonString = circularJson.stringify(group);
+    let groupName = 'No Group';
+    let specialtyName = 'No Specialty';
+    let turnName = 'No Turn';
+    let tutorName = 'No Tutor';
+    let periodName = 'No Period';
+    let asignatureName = 'No asignature';
 
-    // Si jsonString es 'undefined', significa que había una referencia circular
-    if (typeof jsonString !== 'undefined') {
-      const groupWithoutCircularReferences = circularJson.parse(jsonString);
-      console.log("Group without circular references:", groupWithoutCircularReferences);
-      console.log("Result from database:", group);
-      return res.render('perfil.ejs', { group: groupWithoutCircularReferences });
-    } else {
-      return res.render('perfil.ejs', { group: null });
+    if (groupResult && groupResult.id_group) {
+      groupName = groupResult.id_group;
     }
+
+    if (specialtyResult && specialtyResult.specialty) {
+      specialtyName = specialtyResult.specialty;
+    }
+
+    if (turnResult && turnResult.turn) {
+      turnName = turnResult.turn;
+    }
+
+    if (tutorResult && tutorResult.tutor) {
+      tutorName = tutorResult.tutor;
+    }
+
+    if (periodResult && periodResult.id_period) {
+      periodName = periodResult.id_period;
+    }
+
+    if (asignatureResult && asignatureResult.asignature) {
+      asignatureName = asignatureResult.asignature;
+    }
+
+    res.render("perfil", { groupName, specialtyName, studentName, studentEmail, turnName, tutorName, periodName, asignatureName });
   } catch (error) {
-    console.error(error);
-    return res.status(500).render('error', { error });
+    // Manejar errores aquí
+    console.error("Error fetching data:", error);
+    res.render("perfil", { groupName: 'Unknown Group', specialtyName: 'Unknown Specialty', turnName: 'Unknown Turn', tutorName: 'Unknown Tutor', periodName: 'Unknown Period', asignatureName:'Unknown Asignature' });
   }
 };
 
 module.exports = {
-  renderPerfilPage: renderPerfilPage,
+  renderPerfilPage,
 };
--- src/services/studentService.js
+++ src/services/studentService.js
@@ -1,27 +1,115 @@
+const util = require('util');
 const db = require('./dbservice');
+
+const queryAsync = util.promisify(db.query).bind(db);
 
 const getGroupByStudentId = async (studentId) => {
   try {
-    const query = 'SELECT name FROM groups WHERE id_group = (SELECT id_group FROM students WHERE id_student = ?);';
-    console.log('SQL Query:', query);
-
-    // Utiliza await para esperar el resultado de la consulta
-    const result = await db.query(query, [studentId]);
-
-    if (result && result.length > 0) {
-      return result[0].name;
-    } else if (result && result.length === 0) {
-      return "El estudiante no está en un grupo.";
+    const queryResult = await queryAsync('SELECT id_group FROM groups WHERE id_group = (SELECT id_group FROM students WHERE id_student = ?);', [studentId]);
+    
+    if (queryResult.length > 0) {
+      console.log('The group is:', queryResult[0].id_group);
+      return { id_group: queryResult[0].id_group };
     } else {
-      console.error("Error al obtener datos del grupo. Result:", result);
-      return "Error al obtener datos del grupo.";
+      console.log('Student not found or not assigned to any group.');
+      return null;
     }
   } catch (error) {
-    console.error('Error de consulta:', error); // Agrega esta línea
+    console.error('Error executing query:', error);
+    throw error;
+  }
+};
+
+const getSpecialtyByStudentId = async (studentId) => {
+  try {
+    const queryspecialtyResult = await queryAsync('SELECT name FROM cat_specialities WHERE id_specialty = (SELECT id_specialty FROM students WHERE id_student = ?);', [studentId]);
+    
+    if (queryspecialtyResult.length > 0) {
+      console.log('The specialty is:', queryspecialtyResult[0].name);
+      return { specialty: queryspecialtyResult[0].name };
+    } else {
+      console.log('Student not found or not assigned to any Specialty.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error executing query:', error);
+    throw error;
+  }
+};
+
+const getTurnByStudentId = async (studentId) => {
+  try {
+    const queryTurnResult = await queryAsync('SELECT name FROM turns WHERE id_turn = (SELECT id_turn FROM students WHERE id_student = ?);', [studentId]);
+    
+    if (queryTurnResult.length > 0) {
+      console.log('The Turn is:', queryTurnResult[0].name);
+      return { turn: queryTurnResult[0].name }; // Corregido aquí
+    } else {
+      console.log('Student not found or not assigned to any turn.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error executing query:', error);
+    throw error;
+  }
+};
+
+const getTutorByStudentId = async (studentId) => {
+  try {
+    const queryTutorResult = await queryAsync('SELECT fullname FROM teachers WHERE id_teacher = (SELECT id_teacher FROM tutors WHERE id_tutor = (SELECT id_tutor FROM students WHERE id_student = ?));', [studentId]);
+    
+    if (queryTutorResult.length > 0) {
+      console.log('The tutor is:', queryTutorResult[0].fullname); // Corregido aquí
+      return { tutor: queryTutorResult[0].fullname };
+    } else {
+      console.log('Student not found or not assigned to any tutor.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error executing query:', error);
+    throw error;
+  }
+};
+
+const getPeriodByStudentId = async (studentId) => {
+  try {
+    const queryPeriodResult = await queryAsync('SELECT id_period FROM periods WHERE id_Period = (SELECT id_period FROM students WHERE id_student = ?)', [studentId]);
+    
+    if (queryPeriodResult.length > 0) {
+      console.log('The Period is:', queryPeriodResult[0].id_period); // Corregido aquí
+      return { id_period: queryPeriodResult[0].id_period };
+    } else {
+      console.log('Student not found or not assigned to any period.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error executing query:', error);
+    throw error;
+  }
+};
+
+const getAsignatureByStudentId = async (studentId) => {
+  try {
+    const queryAsignatureResult = await queryAsync('SELECT name FROM asignatures WHERE id_asignature = (SELECT id_asignature FROM students WHERE id_student = ?);', [studentId]);
+    
+    if (queryAsignatureResult.length > 0) {
+      console.log('The asignature is:', queryAsignatureResult[0].name);
+      return { asignature: queryAsignatureResult[0].name }; // Corregido aquí
+    } else {
+      console.log('Student not found or not assigned to any asignature.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error executing query:', error);
     throw error;
   }
 };
 
 module.exports = {
   getGroupByStudentId: getGroupByStudentId,
+  getSpecialtyByStudentId: getSpecialtyByStudentId,
+  getTurnByStudentId: getTurnByStudentId,
+  getTutorByStudentId: getTutorByStudentId,
+  getPeriodByStudentId: getPeriodByStudentId,
+  getAsignatureByStudentId: getAsignatureByStudentId
 };
